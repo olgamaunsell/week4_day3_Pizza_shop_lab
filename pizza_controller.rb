36decb7d483dc2683ba428b8cf198454b1d3aaf5
Olga Maunsell <olgamaunsell@yahoo.co.uk>
2017-12-13 14:23:50 +0000

    Initial setup for lab

---
+++ pizza_controller.rb
@@ -0,0 +1,37 @@
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+require( 'pry-byebug' )
+require_relative('./models/pizza.rb')
+
+# INDEX route
+
+get('/pizzas') do
+  @pizzas = Pizza.all()
+  erb(:index)
+
+end
+
+# NEW route
+
+get('/pizzas/new') do
+  erb(:new)
+end
+
+# SHOW route
+
+get('/pizzas/:id') do
+
+  id = params[:id]
+  @pizza = Pizza.find(id)
+  erb(:show)
+end
+
+# CREATE route
+
+post('/pizzas') do
+
+  @pizza = Pizza.new(params)
+  @pizza.save()
+
+  erb(:create)
+end
